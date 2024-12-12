ec47d29fe3d8165f558e7d433efdeece2b51727f
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-12-12 12:11:49 +0200

    Add multiWindows module for threaded window creation and remove customtkinter window implementation

---
+++ src/ui/multiWindows.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+
+import threading
+import time
+
+root = tk.Tk()
+root.withdraw()
+
+
+class CreateWindow:
+    open_windows = 0
+
+    def build(self, x=50, y=50, width=250, height=100):
+        self.w = tk.Toplevel()
+
+        self.w.title("Threaded Window")
+        self.w.configure(bg='#25282c')
+        self.w.grid_columnconfigure(0, weight=1)
+        self.w.grid_columnconfigure(1, weight=1)
+
+        self.w.geometry(
+            f"{width}x{height}+{int(round(width/100*x))}+{int(round(height/100*y))}"
+        )
+
+        self.w.protocol("WM_DELETE_WINDOW", self.on_close)
+        self.w.update()
+        time.sleep(0.2)
+
+    def on_close(self):
+        self.w.destroy()
+        CreateWindow.open_windows -= 1
+
+        print(CreateWindow.open_windows)
+
+        if CreateWindow.open_windows <= 0:
+            root.destroy()
+
+    def __init__(self, x=50, y=50, width=250, height=100):
+        CreateWindow.open_windows += 1
+        thread = threading.Thread(
+            target=self.build(x, y, width, height), daemon=True
+        )
+        thread.start()
+        time.sleep(0.2)
+
+
+def start():
+    root.mainloop()
