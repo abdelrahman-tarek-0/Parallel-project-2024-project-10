703b9867b06727b153a16103ffb5dd6ad0e3919c
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-12-12 12:11:10 +0200

    Refactor widgets.py to replace customtkinter with tkinter for button and frame creation, enhancing compatibility and simplifying UI components

--- src/ui/widgets.py
+++ src/ui/widgets.py
@@ -1,25 +1,22 @@
-import customtkinter
+# import customtkinter
+import tkinter as tk
 
 
 def create_button(app, text, command):
-    button = customtkinter.CTkButton(app, text=text, command=command)
+    button = tk.Button(app, text=text, command=command)
     button.pack(anchor="w", padx=10, pady=10)
     return button
 
 
-def create_data_frame(app, target, row, col, colspan=1):
-    frame = customtkinter.CTkFrame(app, corner_radius=10)
+def create_data_frame(app):
+    frame = tk.Frame(app,  bg="#262626")
 
-    frame.grid(row=row, column=col, columnspan=colspan, padx=10, pady=10, sticky="nsew")
-
-    title_label = customtkinter.CTkLabel(frame, text=target, font=("Arial", 16, "bold"))
-
-    title_label.pack(anchor="w", padx=10, pady=(5, 0))
+    frame.grid(row=0, column=0, padx=10, pady=10, columnspan=2, sticky="nsew")
 
     return frame
 
 
-def create_label(app, text):
-    label = customtkinter.CTkLabel(app, text=text, font=("Arial", 14))
+def create_label(app, text, bg="#262626"):
+    label = tk.Label(app, text=text, font=("Arial", 14), bg=bg, fg="white")
     label.pack(anchor="w", padx=10, pady=5)
     return label
