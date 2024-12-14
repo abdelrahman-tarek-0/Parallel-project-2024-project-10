8b2253c878b8132158db4424f235f9fd8fe8f502
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-12-14 18:38:04 +0200

    Add configuration window for user settings with validation

---
+++ src/ui/configWindow.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+import ui.widgets as widgets
+
+
+def get_configs():
+    configPage = widgets.create_window(300, 400)
+    configPage.title("Configuration")
+    errorLabel = None
+
+    data = {}
+
+    widgets.create_label(configPage, "Number of threaded windows:")
+    numOfThreadsEntry = widgets.crete_entry(configPage, "5")
+
+    widgets.create_label(configPage, "Fake request delay (sec):")
+    fakeRequestDelayEntry = widgets.crete_entry(configPage, "2")
+
+    widgets.create_label(configPage, "Refresh every (sec):")
+    refreshDelayEntry = widgets.crete_entry(configPage, "5")
+
+    def on_confirm():
+        numOfThreads = numOfThreadsEntry.get()
+        fakeRequestDelay = fakeRequestDelayEntry.get()
+        refreshDelay = refreshDelayEntry.get()
+
+        if not numOfThreads or not numOfThreads.isdigit():
+            errorLabel.config(text="Invalid number of threads")
+            return
+        
+        if not fakeRequestDelay or not fakeRequestDelay.isdigit():
+            errorLabel.config(text="Invalid delay")
+            return
+        
+        if not refreshDelay or not refreshDelay.isdigit():
+            errorLabel.config(text="Invalid refresh delay")
+            return
+        
+        data["delay"] = int(fakeRequestDelay)
+        data["refreshDelay"] = int(refreshDelay)
+        data["numOfThreads"] = int(numOfThreads)
+
+        configPage.quit()
+        configPage.destroy()
+
+    confirmButton = widgets.create_button(configPage, "Confirm", on_confirm)
+    confirmButton.place(anchor="center")
+    confirmButton.pack(pady=30)
+
+    errorLabel = widgets.create_label(configPage, "")
+    errorLabel.configure(fg="red")
+    errorLabel.pack()
+
+    configPage.mainloop()
+
+    if not data:
+        exit()
+
+    return data
