0f7cca240ec08323d297f800688228e94d7735b6
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-12-14 18:49:40 +0200

    Add configuration options for number of posts per thread with validation

--- src/ui/configWindow.py
+++ src/ui/configWindow.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import ui.widgets as widgets
 
 
+
+
 def get_configs():
     configPage = widgets.create_window(300, 400)
     configPage.title("Configuration")
@@ -9,19 +11,23 @@ def get_configs():
 
     data = {}
 
-    widgets.create_label(configPage, "Number of threaded windows:")
+    widgets.create_label(configPage, "Number of threaded windows:", bg=widgets.Colors.WINDOW)
     numOfThreadsEntry = widgets.crete_entry(configPage, "5")
 
-    widgets.create_label(configPage, "Fake request delay (sec):")
+    widgets.create_label(configPage, "Fake request delay (sec):", bg=widgets.Colors.WINDOW)
     fakeRequestDelayEntry = widgets.crete_entry(configPage, "2")
 
     widgets.create_label(configPage, "Refresh every (sec):")
     refreshDelayEntry = widgets.crete_entry(configPage, "5")
 
+    widgets.create_label(configPage, "Number of posts per thread:", bg=widgets.Colors.WINDOW)
+    numOfPostsEntry = widgets.crete_entry(configPage, "5")
+
     def on_confirm():
         numOfThreads = numOfThreadsEntry.get()
         fakeRequestDelay = fakeRequestDelayEntry.get()
         refreshDelay = refreshDelayEntry.get()
+        numOfPosts = numOfPostsEntry.get()
 
         if not numOfThreads or not numOfThreads.isdigit():
             errorLabel.config(text="Invalid number of threads")
@@ -35,9 +41,14 @@ def get_configs():
             errorLabel.config(text="Invalid refresh delay")
             return
         
+        if not numOfPosts or not numOfPosts.isdigit():
+            errorLabel.config(text="Invalid number of posts")
+            return
+        
         data["delay"] = int(fakeRequestDelay)
         data["refreshDelay"] = int(refreshDelay)
         data["numOfThreads"] = int(numOfThreads)
+        data["numOfPosts"] = int(numOfPosts)
 
         configPage.quit()
         configPage.destroy()
@@ -46,7 +57,7 @@ def get_configs():
     confirmButton.place(anchor="center")
     confirmButton.pack(pady=30)
 
-    errorLabel = widgets.create_label(configPage, "")
+    errorLabel = widgets.create_label(configPage, "", bg=widgets.Colors.WINDOW)
     errorLabel.configure(fg="red")
     errorLabel.pack()
 
